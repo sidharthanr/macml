ec265d7225a4438e99480e3d4db9f30d88975cde
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-05-01 15:58:13 -0400

    Documentation for improved dataset.

--- R/data.R
+++ R/data.R
@@ -1,8 +1,18 @@
 #' CS Uncorrelated
 #' 
-#' A dataset containing data.
+#' A dataset containing synthetic data from Bhat's website. The dataset has
+#' 5000 individuals choosing between five alternatives with five attributes 
+#' each. The dataset columns are as follows:
 #' 
-#' @format a data frame with 5000 rows and 33 variables.
+#' \itemize{
+#'   \item{id. Agent ID}
+#'   \item{alt. Alternative}
+#'   \item{choice. Chosen alternative}
+#'   \item{a:e. five synthetic attributes}
+#' }
 #' 
+#' @format a \code{tbl_df(data.frame)} with 25,000 and 8 columns.
 #' 
+#' @source \url{}
+#' @name cs_uncorrelated
 "cs_uncorrelated"
