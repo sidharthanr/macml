2e4ed4df4fbb01506541eede79d265f4b8119365
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-05-01 15:43:27 -0400

    started wrapper function

---
+++ R/csrp.R
@@ -0,0 +1,11 @@
+#' Cross sectional random parameters by MACML
+#' 
+#' \code{csrp} estimates a cross-sectional random parameters discrete choice 
+#' model applying the MACML estimator.
+#' 
+#' 
+#' 
+#' @export
+csrp <- function(data){
+  
+}
